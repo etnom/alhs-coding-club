9a51e3cb996e4ddf1f0faa9ac6226d4c9f63229a
Monte <montgomery.okdaily@gmail.com>
2016-10-31 23:50:40 +0000

    this is what we did on day y
    one*. We created some variables and learned about if statements and some operators

--- PhaserIntro/Day1/JS/mainGame.js
+++ PhaserIntro/Day1/JS/mainGame.js
@@ -1,3 +1,6 @@
+var mySprite;
+var myNumber = 1;
+
 var gameVar = {
     preload: function () {
         game.load.image("yayIMG", "Assets/yay.jpg");
@@ -5,9 +8,26 @@ var gameVar = {
     
     create: function() {
         console.log("its working!");
+        
+        mySprite = game.add.sprite(10, 10, "yayIMG");
     }, 
     
     update: function () {
+        // if (myNumber === 1) {
+        //     mySprite.kill();
+        // }
+        
+        if (mySprite.x < 50) {
+            mySprite.x += myNumber;
+            mySprite.y += myNumber;
+        } else if (mySprite.x < 200) {
+            mySprite.x += myNumber * 4;
+            mySprite.x += myNumber * 4;
+        } else {
+            mySprite.x += myNumber * 2;
+            mySprite.y += myNumber * 2;
+        }
+        
         
     }
     
