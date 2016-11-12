63ef1eb3950c697fdb6f1d28791924e870be90cd
BobLakensi <montgomery.okdaily@gmail.com>
2016-11-12 14:39:58 -0800

    create folder for Day2

--- PhaserIntro/Day2/JS/mainGame.js
+++ PhaserIntro/Day2/JS/mainGame.js
@@ -1,53 +1,32 @@
 var mySprite;
-var backGround;
-
-var mySpriteSpeed =10 ;
-
-var keyW, keyA, keyS, keyD;
+var myNumber = 1;
 
 var gameVar = {
     preload: function () {
         game.load.image("yayIMG", "Assets/yay.jpg");
-        game.load.image("backgroundIMG", "Assets/shrekObamaMemeDontOpen.jpg");
-        game.load.image("sexyIMG", "Assets/sexyIMG.jpg");
-        
-        
     }, 
     
     create: function() {
         console.log("its working!");
         
-        keyW = game.input.keyboard.addKey(Phaser.Keyboard.W);
-        keyA = game.input.keyboard.addKey(Phaser.Keyboard.A);
-        keyS = game.input.keyboard.addKey(Phaser.Keyboard.S);
-        keyD = game.input.keyboard.addKey(Phaser.Keyboard.D);
-
-        backGround = game.add.sprite(0, 0, "backgroundIMG");
-        backGround.height = game.height;
-        backGround.width = game.width;
-        
-        mySprite = game.add.sprite(game.width/2, game.height/2, "sexyIMG");
-        
-        mySprite.scale.x = .5;
-        mySprite.scale.y = .5;
-        
-        mySprite.x = game.width/2 - mySprite.width/2;
-        mySprite.y = game.height/2 - mySprite.height/2;
-        
+        mySprite = game.add.sprite(10, 10, "yayIMG");
     }, 
     
     update: function () {
-        if (keyW.isDown) {
-            mySprite.y -= mySpriteSpeed;
-        } else if (keyA.isDown) {
-            mySprite.x -= mySpriteSpeed;
-        } else if (keyS.isDown) {
-            mySprite.y += mySpriteSpeed;
-        } else if (keyD.isDown) {
-            mySprite.x += mySpriteSpeed;
+        // if (myNumber === 1) {
+        //     mySprite.kill();
+        // }
+        
+        if (mySprite.x < 50) {
+            mySprite.x += myNumber;
+            mySprite.y += myNumber;
+        } else if (mySprite.x < 200) {
+            mySprite.x += myNumber * 4;
+            mySprite.x += myNumber * 4;
+        } else {
+            mySprite.x += myNumber * 2;
+            mySprite.y += myNumber * 2;
         }
-        
-        
         
         
     }
